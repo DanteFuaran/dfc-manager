a2fe18b2f16a274111c0e44d151a6ff5c55c52cf
Dante <d.fuaran@gmail.com>
2026-03-28 01:23:54 +0300

    v0.0.49: fix stop message - show 'Скрипт остановлен пользователем'

--- lib/install/subpage.sh
+++ lib/install/subpage.sh
@@ -597,7 +597,7 @@ _installation_subpage_standalone() {
 
     # Устанавливаем trap для удаления при прерывании
     if [ "$is_fresh_install" = true ]; then
-        trap 'echo; echo -e "${RED}Установка прервана пользователем${NC}"; echo; rm -rf "'"${SUBPAGE_DIR}"'" 2>/dev/null; exit 1' INT TERM
+        trap 'rm -rf "'"${SUBPAGE_DIR}"'" 2>/dev/null; echo; echo "Скрипт остановлен пользователем"; echo; exit 130' INT TERM
     fi
 
     # Запрашиваем URL панели
